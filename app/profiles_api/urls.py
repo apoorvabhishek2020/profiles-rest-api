bbe14f3fcc512bfdecf8d6d3cfc462428463c5e3
Apoorv Abhishek <apoorvabhishek2020@gmail.com>
2023-02-12 10:27:47 +0000

    Added ViewSet to Profile_api

--- app/profiles_api/urls.py
+++ app/profiles_api/urls.py
@@ -1,10 +1,16 @@
 """
 This module defines all the urls mappings for profiles_api app.
 """
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view(), name="hello-view"),
+    path('', include(router.urls)),
 ]
